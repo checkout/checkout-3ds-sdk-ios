a120e216eb03a285aa3c8cf31197aed017f8df95
Harry Brown <harry.brown@checkout.com>
2021-09-09 09:55:02 +0100

    PIMOB-236: initial SPM setup

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Checkout3DS",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Checkout3DSWrapper",
+            type: .dynamic,
+            targets: ["Checkout3DSWrapper", "Checkout3DS"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airsidemobile/JOSESwift.git", .exact("2.2.1")),
+        .package(name: "CheckoutEventLoggerKit", url: "https://github.com/checkout/checkout-event-logger-ios-framework.git", .exact("1.0.3"))
+    ],
+    targets: [
+        .target(
+            name: "Checkout3DSWrapper",
+            dependencies: ["Checkout3DS", "CheckoutEventLoggerKit", "JOSESwift"]
+        ),
+        .binaryTarget(
+            name: "Checkout3DS",
+            path: "Checkout3DS.xcframework"
+        ),
+    ]
+)
---
+++ Sources/Checkout3DSWrapper/Mock.swift
@@ -0,0 +1,8 @@
+//
+//  Mock.swift
+//  
+//
+//  Created by Harry.Brown on 09/09/2021.
+//
+
+import Foundation
