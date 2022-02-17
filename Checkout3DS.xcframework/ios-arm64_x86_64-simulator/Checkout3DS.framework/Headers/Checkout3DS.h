c4a125fffd722a9dd4dc82e0b38c318ac4e070aa
Harry Brown <harry.brown@checkout.com>
2022-02-17 16:53:21 +0000

    M1 support xcframework

---
+++ Checkout3DS.xcframework/ios-arm64_x86_64-simulator/Checkout3DS.framework/Headers/Checkout3DS.h
@@ -0,0 +1,15 @@
+//
+//  Checkout3DS.h
+//  Checkout3DS
+//
+//  Created by Daven.Gomes on 11/02/2020.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Checkout3DS.
+FOUNDATION_EXPORT double Checkout3DSVersionNumber;
+
+//! Project version string for Checkout3DS.
+FOUNDATION_EXPORT const unsigned char Checkout3DSVersionString[];
+
