99a580957416c23a37c226afab2823878a393383
Ehab Al <ehab.alsharkawy@checkout.com>
2023-06-30 10:02:27 +0100

    Add dynamic type

--- Package.swift
+++ Package.swift
@@ -10,10 +10,31 @@ let package = Package(
     products: [
         .library(
             name: "Checkout3DS",
+            type: .dynamic,
             targets: ["Checkout3DS"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+            url: "https://github.com/checkout/checkout-event-logger-ios-framework",
+            revision: "1.2.4"),
+        .package(
+            url: "https://github.com/airsidemobile/JOSESwift",
+            revision: "2.4.0")
+    ],
     targets: [
+        .target(
+            name: "Checkout3DSWrapper",
+            dependencies: [
+                .target(name: "Checkout3DS"),
+                .product(
+                    name: "CheckoutEventLoggerKit",
+                    package: "checkout-event-logger-ios-framework"),
+                .product(
+                    name: "JOSESwift",
+                    package: "JOSESwift")
+            ],
+            path: "Wrapper"
+        ),
         .binaryTarget(
             name: "Checkout3DS",
             path: "Checkout3DS.xcframework"
