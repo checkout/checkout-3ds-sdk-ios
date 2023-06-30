c2b8a09772decbe7b89422bc704a59b0b09a83c7
Ehab Al <ehab.alsharkawy@checkout.com>
2023-06-30 16:20:26 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -3,42 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "Checkout3DS",
-    defaultLocalization: "en",
     platforms: [
         .iOS(.v12)
     ],
     products: [
-        .library(
-            name: "Checkout3DS",
-            type: .dynamic,
-            targets: ["Checkout3DS"])
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/checkout/checkout-event-logger-ios-framework",
-            revision: "1.2.4"),
-        .package(
-            url: "https://github.com/airsidemobile/JOSESwift",
-            revision: "2.4.0")
+        .library( name: "Checkout3DS", targets: ["Checkout3DS"])
     ],
     targets: [
-        .target(
-            name: "Checkout3DSWrapper",
-            dependencies: [
-                .target(name: "Checkout3DS"),
-                .product(
-                    name: "CheckoutEventLoggerKit",
-                    package: "checkout-event-logger-ios-framework"),
-                .product(
-                    name: "JOSESwift",
-                    package: "JOSESwift")
-            ],
-            path: "Wrapper"
-        ),
-        .binaryTarget(
-            name: "Checkout3DS",
-            path: "Checkout3DS.xcframework"
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+        .binaryTarget( name: "Checkout3DS", url: "https://github.com/checkout/checkout-3ds-sdk-ios/archive/refs/tags/3.0.0.zip" )
+    ]
 )
