bfd541174d57b0386cba6d54dceab1549ecaf874
Ehab Al <ehab.alsharkawy@checkout.com>
2023-07-11 20:57:47 +0100

    Add dummy wrapper

--- Package.swift
+++ Package.swift
@@ -2,19 +2,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Checkout3DSWrapper",
+    name: "Checkout3DS",
     platforms: [
         .iOS(.v12)
     ],
    products: [
         .library(
             name: "Checkout3DSWrapper",
-            targets: ["Checkout3DSWrapper"]),
+            targets: ["Checkout3DSWrapper", "Checkout3DS"]
+        )
     ],
     dependencies: [
-         .package(
-            url: "https://github.com/checkout/checkout-3ds-sdk-ios",
-            branch: "feature/PIMOB-2035_support_spm"),
         .package(
             url: "https://github.com/checkout/checkout-event-logger-ios-framework",
             revision: "1.2.0"),
@@ -27,16 +25,17 @@ let package = Package(
             name: "Checkout3DSWrapper",
             dependencies: [
                 .product(
-                    name: "Checkout3DS",
-                    package: "checkout-3ds-sdk-ios"),
-                .product(
                     name: "CheckoutEventLoggerKit",
                     package: "checkout-event-logger-ios-framework"),
                 .product(
                     name: "JOSESwift",
                     package: "JOSESwift")
-            ],
-            path: "Checkout3DSWrapper"
+                ]
+            ),
+        .binaryTarget(
+            name: "Checkout3DS",
+            url: "https://github.com/checkout/checkout-3ds-sdk-ios/releases/download/3.0.0/Checkout3DS.xcframework.zip",
+            checksum: "5e899dc2f7ac7be3668c1347b0633ab33f6b2ac93eccc26015ca87410ccb6a02"
         )
     ]
 )
