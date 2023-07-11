e67bf0c2f5595fc769e6a8085397987036c2de41
Ehab Al <ehab.alsharkawy@checkout.com>
2023-07-11 21:47:46 +0100

    Add framework as binary

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -6,36 +6,17 @@ let package = Package(
     platforms: [
         .iOS(.v12)
     ],
-   products: [
+    products: [
         .library(
-            name: "Checkout3DSWrapper",
-            targets: ["Checkout3DSWrapper", "Checkout3DS"]
+            name: "Checkout3DS",
+            targets: ["Checkout3DS"]
         )
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/checkout/checkout-event-logger-ios-framework",
-            revision: "1.2.0"),
-        .package(
-            url: "https://github.com/airsidemobile/JOSESwift",
-            revision: "2.4.0")
-    ],
+    dependencies: [],
     targets: [
-        .target(
-            name: "Checkout3DSWrapper",
-            dependencies: [
-                .product(
-                    name: "CheckoutEventLoggerKit",
-                    package: "checkout-event-logger-ios-framework"),
-                .product(
-                    name: "JOSESwift",
-                    package: "JOSESwift")
-                ]
-            ),
         .binaryTarget(
             name: "Checkout3DS",
-            url: "https://github.com/checkout/checkout-3ds-sdk-ios/releases/download/3.0.0/Checkout3DS.xcframework.zip",
-            checksum: "5e899dc2f7ac7be3668c1347b0633ab33f6b2ac93eccc26015ca87410ccb6a02"
-        )
+            path: "Checkout3DS.xcframework"
+        ),
     ]
 )
