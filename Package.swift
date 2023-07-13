049dbe9f4e1d073d495cb74629423f29802b8146
Ehab Al <ehab.alsharkawy@checkout.com>
2023-07-13 16:42:14 +0100

    Add missing files

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 
 import PackageDescription
 let package = Package(
-    name: "Checkout3DS",
+    name: "Checkout3DSTargets",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         .library(
-            name: "Checkout3DS",
+            name: "Checkout3DSTargets",
             targets: ["Checkout3DSTargets"]
         )
     ],
@@ -21,9 +21,15 @@ let package = Package(
             name: "JOSESwift",
             path: "JOSESwift.xcframework"
         ),
+    .binaryTarget(
+            name: "CheckoutEventLoggerKit",
+            url: "https://github.com/checkout/checkout-event-logger-ios-framework/releases/download/1.2.4/CheckoutEventLoggerKit.xcframework.zip",
+            checksum: "5db418477fc07baef7acceeb0e831f00872bc4fc9ba980e039ac1684cecbc145"
+        ),
         .target(name: "Checkout3DSTargets",
                 dependencies: [
                     .target(name: "JOSESwift", condition: .when(platforms: .some([.iOS]))),
+                    .target(name: "CheckoutEventLoggerKit", condition: .when(platforms: .some([.iOS]))),
                     .target(name: "Checkout3DS", condition: .when(platforms: .some([.iOS])))
                 ],
                 path: "Checkout3DSTargets"
