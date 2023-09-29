5e532c8ba68eade6b2f30d6af921617722b29148
Ehab Al <ehab.alsharkawy@checkout.com>
2023-09-29 18:14:47 +0100

    Update packages

--- Package.swift
+++ Package.swift
@@ -2,20 +2,24 @@
 
 import PackageDescription
 let package = Package(
-    name: "Checkout3DSTargets",
+    name: "Checkout3DSPackages",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         .library(
-            name: "Checkout3DSTargets",
-            targets: ["Checkout3DSTargets"]
+            name: "Checkout3DSPackages",
+            targets: ["Checkout3DSPackages"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "Checkout3DS",
             path: "Checkout3DS.xcframework"
+        ),
+        .binaryTarget(
+            name: "Checkout3DS-Security",
+            path: "Checkout3DS-Security.xcframework"
         ),
         .binaryTarget(
             name: "JOSESwift",
@@ -26,13 +30,14 @@ let package = Package(
             url: "https://github.com/checkout/checkout-event-logger-ios-framework/releases/download/1.2.4/CheckoutEventLoggerKit.xcframework.zip",
             checksum: "5db418477fc07baef7acceeb0e831f00872bc4fc9ba980e039ac1684cecbc145"
         ),
-        .target(name: "Checkout3DSTargets",
+        .target(name: "Checkout3DSPackages",
                 dependencies: [
                     .target(name: "JOSESwift", condition: .when(platforms: .some([.iOS]))),
                     .target(name: "CheckoutEventLoggerKit", condition: .when(platforms: .some([.iOS]))),
-                    .target(name: "Checkout3DS", condition: .when(platforms: .some([.iOS])))
+                    .target(name: "Checkout3DS", condition: .when(platforms: .some([.iOS]))),
+                    .target(name: "Checkout3DS-Security", condition: .when(platforms: .some([.iOS])))
                 ],
-                path: "Checkout3DSTargets"
+                path: "Checkout3DSPackages"
         )
     ],
     swiftLanguageVersions: [.v5]
