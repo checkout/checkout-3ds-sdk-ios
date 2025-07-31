5ffcb9412721273fba2087bb7a1065be5c297558
Ehab Al <ehab.alsharkawy@checkout.com>
2025-07-31 15:05:06 +0100

    Remove sonar analysis

--- .github/scripts/run_sonar.sh
+++
@@ -1,9 +0,0 @@
-sonar-scanner \
-    -Dsonar.organization=checkout-ltd \
-    -Dsonar.projectKey=checkout_checkout-3ds-sdk-ios \
-    -Dsonar.sources=. \
-    -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.cfamily.build-wrapper-output.bypass=true \
-    -Dsonar.c.file.suffixes=- \
-    -Dsonar.cpp.file.suffixes=- \
-    -Dsonar.objc.file.suffixes=-
