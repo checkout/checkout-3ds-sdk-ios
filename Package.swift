2633eeea697bce29fdf06e0f8ccfb7e880986602
Roman Chugunov <roman.chugunov@checkout.com>
2026-03-09 14:03:15 +0000

    New version 3.3.0

--- Package.swift
+++ Package.swift
@@ -22,10 +22,6 @@ let package = Package(
             name: "Checkout3DS",
             path: "Checkout3DS.xcframework"
         ),
-        .binaryTarget(
-            name: "Checkout3DS-Security",
-            path: "Checkout3DS-Security.xcframework"
-        ),
         .target(
             name: "JOSESwiftDynamic",
             dependencies: ["JOSESwift"],
@@ -33,14 +29,15 @@ let package = Package(
         ),
         .target(name: "Checkout3DSPackages",
                 dependencies: [
-                    .product(name: "CheckoutEventLoggerKit", package: "checkout-event-logger-ios-framework"),
+                    .product(
+                        name: "CheckoutEventLoggerKit",
+                        package: "checkout-event-logger-ios-framework"
+                    ),
                     "JOSESwiftDynamic",
-                    .target(name: "Checkout3DS",
-                            condition: .when(platforms: [.iOS])),
-                    .target(name: "Checkout3DS-Security",
-                            condition: .when(platforms: [.iOS]))
+                    .target(name: "Checkout3DS", condition: .when(platforms: [.iOS])),
                 ],
-                path: "Checkout3DSPackages")
+                path: "Checkout3DSPackages"
+           )
     ],
     swiftLanguageVersions: [.v5]
 )
